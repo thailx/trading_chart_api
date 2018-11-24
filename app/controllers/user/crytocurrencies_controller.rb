e3876d2118d6530aef1d60213cd1611d1f0cc9dc
Tony Pham <kyo.shenshie@gmail.com>
2018-11-24 11:53:14 +0700

    Add params per_page

--- app/controllers/user/crytocurrencies_controller.rb
+++ app/controllers/user/crytocurrencies_controller.rb
@@ -1,6 +1,6 @@
 class User::CrytocurrenciesController < ApplicationController
   def index
-    @data = Crytocurrency.all
+    @data = Crytocurrency.all.limit(params[:per_page] || 30)
     render json: CrytocurrencySerializer.new(@data).serialized_json
   end
 end
