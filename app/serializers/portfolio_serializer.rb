670025498af8535862456a9cf904779d8a03e4a4
Tony Pham <kyo.shenshie@gmail.com>
2018-12-26 00:54:59 +0700

    Add portfolio default when serializer

--- app/serializers/portfolio_serializer.rb
+++ app/serializers/portfolio_serializer.rb
@@ -1,6 +1,6 @@
 class PortfolioSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :user_id
+  attributes :name, :user_id, :default_portfolio
   has_many :portfolio_items
   belongs_to :user
 end
